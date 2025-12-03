70b9f75cd533e987c64c6d7d95fa736725b61589
Angeline Tamayo <tamayo.angeline@gmail.com>
2025-12-02 22:18:45 -0500

    :sparkles: Day 2 Part 2

--- 2.exs
+++ 2.exs
@@ -1,21 +1,10 @@
 defmodule Go do
+  # Part 1
   def go1() do
-    goo1(parse_file!(), 0)
+    goo(parse_file!(), &is_invalid1/1, 0)
   end
 
-  def goo1([], acc), do: acc
-  def goo1([range | next], acc) do
-    sum_invalid_ids = gooo1(Enum.to_list(range), 0)
-    goo1(next, acc + sum_invalid_ids)
-  end
-
-  def gooo1([], acc), do: acc
-  def gooo1([id | next], acc) do
-    add = if is_invalid(id), do: id, else: 0
-    gooo1(next, acc + add)
-  end
-
-  def is_invalid(id) do
+  def is_invalid1(id) do
     id_str = Integer.to_string(id)
     len = String.length(id_str)
 
@@ -27,6 +16,52 @@ defmodule Go do
     else
       false
     end
+  end
+
+  # Part 2
+  def go2() do
+    goo(parse_file!(), &is_invalid2/1, 0)
+  end
+
+  def is_invalid2(id) do
+    id_str = Integer.to_string(id)
+    len = String.length(id_str)
+
+    len > 0 && is_repeated(id_str, len, len - 1)
+  end
+
+  def is_repeated(_, _, 0), do: false
+  def is_repeated(str, len, maybe_factor) do
+    if Integer.mod(len, maybe_factor) == 0 do
+      factor1 = maybe_factor
+      factor2 = div(len, factor1)
+      {substr, _} = String.split_at(str, factor1)
+      if repeat(substr, factor2) == str do
+        true
+      else
+        is_repeated(str, len, maybe_factor - 1)
+      end
+    else
+      is_repeated(str, len, maybe_factor - 1)
+    end
+  end
+
+  def repeat(str, n), do: append(str, n, "")
+
+  def append(_, 0, acc), do: acc
+  def append(str, n, acc), do: append(str, n - 1, acc <> str)
+
+  # Shared
+  def goo([], _, acc), do: acc
+  def goo([range | next], p, acc) do
+    sum_invalid_ids = gooo(Enum.to_list(range), p, 0)
+    goo(next, p, acc + sum_invalid_ids)
+  end
+
+  def gooo([], _, acc), do: acc
+  def gooo([id | next], p, acc) do
+    add = if p.(id), do: id, else: 0
+    gooo(next, p, acc + add)
   end
 
   def parse_file!() do
@@ -47,3 +82,4 @@ defmodule Go do
 end
 
 IO.puts(Go.go1())
+IO.puts(Go.go2())
