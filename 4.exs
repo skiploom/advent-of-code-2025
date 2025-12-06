78d8b03cb3bf9a9c45eee689652f4a6a227fe9fe
Angeline Tamayo <tamayo.angeline@gmail.com>
2025-12-06 17:45:15 -0500

    :sparkles: Day 4 Part 2

--- 4.exs
+++ 4.exs
@@ -3,6 +3,7 @@ defmodule Go do
   @type grid() :: %{coord() => is_roll :: boolean()}
   @type bounds() :: %{x0: non_neg_integer(), xm: non_neg_integer(), y0: non_neg_integer(), y1: non_neg_integer()}
 
+  # Part 1
   def go1() do
     grid = parse_file!()
     bounds = get_bounds(grid)
@@ -11,7 +12,7 @@ defmodule Go do
 
   def fork(grid, bounds) do
     bounds.y0..bounds.ym
-    |> Enum.map(fn y -> fork_x?(grid, bounds, 0, y, []) end)
+    |> Enum.map(fn y -> fork_x?(grid, bounds, bounds.x0, y, []) end)
     |> List.flatten()
     |> Enum.count(&(&1))
   end
@@ -21,6 +22,44 @@ defmodule Go do
     fork_x?(grid, bounds, x + 1, y, [fork_at?(x, y, grid) | acc])
   end
 
+  # Part 2
+  def go2() do
+    grid = parse_file!()
+    bounds = get_bounds(grid)
+    fortnite(grid, bounds, 0)
+  end
+
+  # Count forky-workys, clean 'em up, then do it all over again
+  def fortnite(grid, bounds, total_forks) do
+    coords = find_forks(grid, bounds)
+    num_forks = MapSet.size(coords)
+
+    if num_forks > 0 do
+      cleaned =
+        Enum.reduce(MapSet.to_list(coords), grid, fn coord, acc ->
+          Map.put(acc, coord, false)
+        end)
+
+      fortnite(cleaned, bounds, total_forks + num_forks)
+    else
+      total_forks
+    end
+  end
+
+  @spec find_forks(grid(), bounds()) :: MapSet.t(coord())
+  def find_forks(grid, bounds) do
+    Enum.reduce(bounds.y0..bounds.ym, MapSet.new(), fn y, acc ->
+      MapSet.union(acc, find_forks_x(grid, bounds, bounds.x0, y, MapSet.new()))
+    end)
+  end
+
+  def find_forks_x(_, %{xm: xm}, x, _, acc) when x == xm + 1, do: acc
+  def find_forks_x(grid, bounds, x, y, acc) do
+    new_acc = if fork_at?(x, y, grid), do: MapSet.put(acc, {x,y}), else: acc
+    find_forks_x(grid, bounds, x + 1, y, new_acc)
+  end
+
+  # Shared
   def fork_at?(x, y, grid) do
     # If I really wanted to be efficient, I could store 0s and 1s in the grid where
     # 1 denotes a roll. Then instead of building up a list and counting here,
@@ -65,3 +104,4 @@ defmodule Go do
 end
 
 IO.puts(Go.go1())
+IO.puts(Go.go2())
