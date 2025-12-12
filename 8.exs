56ab6c5ab31786e023e9083a605836939cf2b316
Angeline Tamayo <tamayo.angeline@gmail.com>
2025-12-12 00:22:36 -0500

    :sparkles: Day 8 Part 2

--- 8.exs
+++ 8.exs
@@ -1,4 +1,5 @@
 defmodule Go do
+  # Part 1
   def go1() do
     num_connections = 1000
 
@@ -18,35 +19,11 @@ defmodule Go do
     |> Enum.reduce(1, &*/2)
   end
 
-  # Calculate distances between all pairs
-  def dists(ps), do: distss(ps, [])
-
-  def distss([p1, p2], acc), do: [{{p1, p2}, dist(p1, p2)} | acc]
-
-  def distss([p1 | rest], acc) do
-    distss(rest, distsss(p1, rest, []) ++ acc)
-  end
-
-  def distsss(_, [], acc), do: acc
-  def distsss(p1, [p2 | rest], acc), do: distsss(p1, rest, [{{p1, p2}, dist(p1, p2)} | acc])
-
-  # Calculate distance between a pair
-  def dist({x1, y1, z1}, {x2, y2, z2}) do
-    ((x1 - x2) ** 2 + (y1 - y2) ** 2 + (z1 - z2) ** 2) ** 0.5
-  end
-
   def connectAll!(pairs, boxes) do
     boxToCircuits = mkBoxToCircuits(boxes, 1, %{})
     circuitToContents = mkCircuitToContents(boxToCircuits)
 
     connect!(pairs, boxToCircuits, circuitToContents)
-  end
-
-  def mkBoxToCircuits([], _, acc), do: acc
-  def mkBoxToCircuits([b | rest], c, acc), do: mkBoxToCircuits(rest, c + 1, Map.put(acc, b, c))
-
-  def mkCircuitToContents(boxToCircuits) do
-    Map.new(boxToCircuits, fn {b, c} -> {c, MapSet.new([b])} end)
   end
 
   def connect!([], _, circuitToContents), do: circuitToContents
@@ -80,6 +57,89 @@ defmodule Go do
     end
   end
 
+  # Part 2
+  def go2() do
+    boxes = parse_file!()
+
+    {pairs, _} =
+      boxes
+      |> dists()
+      |> Enum.sort_by(fn {_, dist} -> dist end)
+      |> Enum.unzip()
+
+    {{x1, _, _}, {x2, _, _}} = actuallyConnectAll!(pairs, boxes)
+    x1 * x2
+  end
+
+  def actuallyConnectAll!(pairs, boxes) do
+    boxToCircuits = mkBoxToCircuits(boxes, 1, %{})
+    circuitToContents = mkCircuitToContents(boxToCircuits)
+
+    megaConnect!(pairs, boxToCircuits, circuitToContents)
+  end
+
+  # This is almost exactly the same as connect!/3 from Part 1.
+  # This year, I don't really want to DRY up code between the parts or
+  # change anything from the previous part, so that it's easier for me
+  # to see exactly what I thought of for each problem.
+  def megaConnect!([{b1, b2} | rest], boxToCircuits, circuitToContents) do
+    case {Map.get(boxToCircuits, b1), Map.get(boxToCircuits, b2)} do
+      {nil, _} ->
+        raise("oop")
+
+      {_, nil} ->
+        raise("oop")
+
+      {same, same} ->
+        megaConnect!(rest, boxToCircuits, circuitToContents)
+
+      {old, new} ->
+        newBoxToCircuits =
+          Map.merge(
+            boxToCircuits,
+            Map.from_keys(MapSet.to_list(Map.get(circuitToContents, old)), new)
+          )
+
+        merged = MapSet.union(Map.get(circuitToContents, old), Map.get(circuitToContents, new))
+
+        newCircuitToContents =
+          circuitToContents
+          |> Map.replace!(new, merged)
+          |> Map.delete(old)
+
+        if map_size(newCircuitToContents) == 1 do
+          {b1, b2}
+        else
+          megaConnect!(rest, newBoxToCircuits, newCircuitToContents)
+        end
+    end
+  end
+
+  # Shared
+  # Calculate distances between all pairs
+  def dists(ps), do: distss(ps, [])
+
+  def distss([p1, p2], acc), do: [{{p1, p2}, dist(p1, p2)} | acc]
+
+  def distss([p1 | rest], acc) do
+    distss(rest, distsss(p1, rest, []) ++ acc)
+  end
+
+  def distsss(_, [], acc), do: acc
+  def distsss(p1, [p2 | rest], acc), do: distsss(p1, rest, [{{p1, p2}, dist(p1, p2)} | acc])
+
+  # Calculate distance between a pair
+  def dist({x1, y1, z1}, {x2, y2, z2}) do
+    ((x1 - x2) ** 2 + (y1 - y2) ** 2 + (z1 - z2) ** 2) ** 0.5
+  end
+
+  def mkBoxToCircuits([], _, acc), do: acc
+  def mkBoxToCircuits([b | rest], c, acc), do: mkBoxToCircuits(rest, c + 1, Map.put(acc, b, c))
+
+  def mkCircuitToContents(boxToCircuits) do
+    Map.new(boxToCircuits, fn {b, c} -> {c, MapSet.new([b])} end)
+  end
+
   def parse_file!() do
     "inputs/8.txt"
     |> File.read!()
@@ -97,3 +157,4 @@ defmodule Go do
 end
 
 IO.puts(Go.go1())
+IO.puts(Go.go2())
